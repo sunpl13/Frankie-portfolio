3e91b28197a5e68f9b843bc9d5ce5005cecb2edc
Kim JungSoo <sunpl13@naver.com>
2024-03-04 00:48:19 +0900

    feat: 반원 컴포넌트 UI

---
+++ src/app/components/profile/HalfCircle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HalfCircle = () => {
+  return <div className="absolute bottom-0 w-224 h-112 rounded-b-none rounded-t-224 bg-green-100"></div>;
+};
+
+export default HalfCircle;
