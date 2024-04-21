08b85ae8afe9ea9319945bcd32562a878deb11a1
Kim JungSoo <sunpl13@naver.com>
2024-04-21 21:05:57 +0900

    feat: 설명 반원 컴포넌트 추가

---
+++ src/app/components/Description/TwoCircle.tsx
@@ -0,0 +1,16 @@
+const TwoCircle = () => {
+  return (
+    <div className="relative border-[1.5px] w-[366px] h-[366px] border-[#dcdede]/[.3] rounded-full">
+      <div className="w-[332px] h-[332px] border-[3px] rounded-full absolute top-[17px] border-[#dcdede]/[.3] left-[17px] border border-dotted flex flex-col justify-center items-center">
+        <strong
+          style={{ color: 'rgba(240,240,240,0.9)' }}
+          className="text-gray-90 font-calibre font-semibold text-4xl">
+          Front-end
+        </strong>
+        <span>프론트엔드</span>
+      </div>
+    </div>
+  );
+};
+
+export default TwoCircle;
