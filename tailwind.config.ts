53e9a46ba877cc49834963f4f988eb404edca319
Kim JungSoo <sunpl13@naver.com>
2024-04-21 21:04:58 +0900

    chore: 테일윈드 추가설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,53 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
       fontFamily: {
-        pretendard: "var(--font-pretendard)",
-        calibre: "var(--font-calibre)",
+        pretendard: 'var(--font-pretendard)',
+        calibre: 'var(--font-calibre)'
       },
       colors: {
-        "green-100": "#98f211",
-        "gray-40": "rgba(255, 255, 255, 0.4)",
-        "gray-60": "rgba(240, 240, 240, 0.6)",
-        "gray-30": "rgba(240, 240, 240, 0.3)",
-        "black-80": "#363636",
+        'green-100': '#98f211',
+        'gray-40': 'rgba(255, 255, 255, 0.4)',
+        'gray-60': 'rgba(240, 240, 240, 0.6)',
+        'gray-30': 'rgba(240, 240, 240, 0.3)',
+        'gray-50': 'rgba(240, 240, 240, 0.5)',
+        'gray-90': 'rgba(240, 240, 240, 0.9)',
+        'black-80': '#363636',
+        'base-dark': '#191919'
       },
       fontSize: {
-        "6.5xl": "6.5625rem",
+        '6.5xl': '6.5625rem'
       },
       width: {
-        "224": "56rem",
-        "3/10": "30%",
+        '224': '56rem',
+        '3/10': '30%'
       },
       height: {
-        "112": "28rem",
+        '112': '28rem'
       },
       borderRadius: {
-        "224": "56rem",
+        '224': '56rem'
       },
-    },
+      backgroundColor: {
+        'base-dark': '#191919'
+      },
+      padding: {
+        '37.5': '9.375rem'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
