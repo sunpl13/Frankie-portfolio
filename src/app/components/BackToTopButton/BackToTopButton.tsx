4f78e3951d41a1dd48161b3f561ec5a08d47d7c0
Jungsoo <sunpl13@naver.com>
2024-05-09 00:20:09 +0900

    feat: 맨 위로 버튼 구현

---
+++ src/app/components/BackToTopButton/BackToTopButton.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { useEffect, useState } from 'react';
+import ArrowUpIcon from '@/assets/icons/arrow-up.svg';
+
+const BackToTopButton = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  useEffect(() => {
+    const onShowButtonClick = () => {
+      if (window.scrollY > 800) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+    window.addEventListener('scroll', onShowButtonClick);
+    return () => {
+      window.removeEventListener('scroll', onShowButtonClick);
+    };
+  }, []);
+  return (
+    <button
+      type="button"
+      aria-label="맨 위로"
+      onClick={scrollToTop}
+      className={`bg-green-100 fixed bottom-14 right-14 outline-none border-none w-14  rounded-full flex justify-center items-center transition duration-300  ${
+        showButton ? 'visible h-14' : 'invisible h-0'
+      }`}>
+      <ArrowUpIcon width="31" height="45" />
+    </button>
+  );
+};
+
+export default BackToTopButton;
