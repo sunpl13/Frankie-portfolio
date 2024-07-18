03149179f9eb3dc16fed5ff9e0506088ba6193fb
Jungsoo <sunpl13@naver.com>
2024-07-18 15:39:32 +0900

    feat: 메타데이터 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,33 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: "Jungsoo's PortFolio",
-  description: "Welcome Frankie's Folio!"
+  description: '3년차 프론트엔드 개발자 김정수 포트폴리오입니다.',
+  applicationName: '김정수 포트폴리오',
+  keywords: [
+    '프론트엔드 포트폴리오',
+    '프론트엔드',
+    '프론트엔드 개발자 포트폴리오',
+    '개발자 포트폴리오',
+    '개발자 이력서'
+  ],
+  creator: 'Jungsoo Kim',
+  authors: [
+    { name: 'Jungsoo Kim', url: 'https://www.jungsookim-portfolio.com' }
+  ],
+  openGraph: {
+    title: '프론트엔드 개발자 김정수입니다.',
+    description: '3년차 프론트엔드 개발자 김정수 포트폴리오입니다.',
+    url: 'https://www.jungsookim-portfolio.com',
+    siteName: "Jungsoo's PortFolio",
+    images: [
+      {
+        url: 'https://www.jungsookim-portfolio.com/images/opengraph.png',
+        alt: '김정수 프로필 사진',
+        width: 800,
+        height: 600
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
